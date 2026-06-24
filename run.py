779e19b8cf2f9ca04f04bcf1231c93158dd65cc9
sayan706 <sayanchatterjee706@gmail.com>
2026-06-24 14:08:53 +0530

    port number added in run.py for render host

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 import gevent.monkey
 gevent.monkey.patch_all()
 
+import os
 from app import create_app, socketio
 
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, use_reloader=False, port=5000, allow_unsafe_werkzeug=True)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=True, use_reloader=False, allow_unsafe_werkzeug=True)
